8089bd804dbf8b9147218f33b95a372538a53d0e
annalatyeva <anysha_s@mail.ru>
2024-05-02 20:20:17 +0500

    add playsInline to portfolio

--- src/components/PortfolioItem/PortfolioItem.jsx
+++ src/components/PortfolioItem/PortfolioItem.jsx
@@ -1,38 +1,33 @@
-import { useState } from "react";
+import {useState} from "react";
 import style from "../PortfolioItem/PortfolioItem.module.scss";
 
-const PortfolioItem = ({ link, number, description, format, cover }) => {
-  const [loaded, setLoaded] = useState(false);
-  const handleLoaded = () => {
-    setLoaded(true);
-  };
-  return (
-    <div className={style.container}>
-      <div className={style.wrapper}>
-        <div className={style.img_wrapper}>
-          {format === "image" ? (
-            <img src={link} alt={`project${number}`} />
-          ) : (
-            <div>
-              {!loaded && <img src={cover} alt={`project${number}`} />}
-              <video
-                autoPlay
-                loop
-                onLoadedData={handleLoaded}
-                className={!loaded && style.notloaded}
-              >
-                <source src={link} type="video/mp4" />
-                Your browser does not support the video tag.
-              </video>
-            </div>
-          )}
-        </div>
-        <span className={style.number}>/</span>
-        <p className={style.number}>{number}</p>
-      </div>
-      <p className={style.description}>{description}</p>
-    </div>
-  );
+const PortfolioItem = ({link, number, description, format, cover}) => {
+	const [loaded, setLoaded] = useState(false);
+	const handleLoaded = () => {
+		setLoaded(true);
+	};
+	return (
+		<div className={style.container}>
+			<div className={style.wrapper}>
+				<div className={style.img_wrapper}>
+					{format === "image" ? (
+						<img src={link} alt={`project${number}`} />
+					) : (
+						<div>
+							{!loaded && <img src={cover} alt={`project${number}`} />}
+							<video autoPlay loop onLoadedData={handleLoaded} playsInline className={!loaded && style.notloaded}>
+								<source src={link} type="video/mp4" />
+								Your browser does not support the video tag.
+							</video>
+						</div>
+					)}
+				</div>
+				<span className={style.number}>/</span>
+				<p className={style.number}>{number}</p>
+			</div>
+			<p className={style.description}>{description}</p>
+		</div>
+	);
 };
 
 export default PortfolioItem;
